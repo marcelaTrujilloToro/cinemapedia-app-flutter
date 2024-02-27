fc9cf3db881afcca35b2f89c337c548f8d22981c
Marcela Trujillo Toro <marces.ttblue@gmail.com>
2024-02-27 17:24:11 -0500

    feat: working on movie details domain and infrastucture

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -53,5 +54,19 @@ class MovieDBDatasource extends MoviesDatasource {
     final response =
         await dio.get('/movie/upcoming', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get('/movie/$id');
+
+    if (response.statusCode != 200)
+      throw Exception('Movie with id: $id not found');
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,27 @@ class MovieMapper {
         posterPath: movieDb.posterPath != ''
             ? 'https://image.tmdb.org/t/p/w500/${movieDb.posterPath}'
             : 'no-poster',
+        releaseDate: movieDb.releaseDate,
+        title: movieDb.title,
+        video: movieDb.video,
+        voteAverage: movieDb.voteAverage,
+        voteCount: movieDb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movieDb) => Movie(
+        adult: movieDb.adult,
+        backdropPath: movieDb.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movieDb.backdropPath}'
+            : 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Fmax_1200%2Ffb3ef66312333.5691dd2253378.jpg&tbnid=_mLFMesCfZusyM&vet=12ahUKEwiAvrP65cmEAxXTYjABHa6jACEQxiAoCnoECAAQKg..i&imgrefurl=https%3A%2F%2Fwww.behance.net%2Fgallery%2F6312333%2FFile-Not-Found-Movie-Poster%3Flocale%3Dcs_CZ&docid=C87rPtPiG_cp5M&w=1200&h=1801&itg=1&q=no%20poster%20found&ved=2ahUKEwiAvrP65cmEAxXTYjABHa6jACEQxiAoCnoECAAQKg',
+        genreIds: movieDb.genres.map((e) => e.name).toList(),
+        id: movieDb.id,
+        originalLanguage: movieDb.originalLanguage,
+        originalTitle: movieDb.originalTitle,
+        overview: movieDb.overview,
+        popularity: movieDb.popularity,
+        posterPath: movieDb.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${movieDb.posterPath}'
+            : 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Fmax_1200%2Ffb3ef66312333.5691dd2253378.jpg&tbnid=_mLFMesCfZusyM&vet=12ahUKEwiAvrP65cmEAxXTYjABHa6jACEQxiAoCnoECAAQKg..i&imgrefurl=https%3A%2F%2Fwww.behance.net%2Fgallery%2F6312333%2FFile-Not-Found-Movie-Poster%3Flocale%3Dcs_CZ&docid=C87rPtPiG_cp5M&w=1200&h=1801&itg=1&q=no%20poster%20found&ved=2ahUKEwiAvrP65cmEAxXTYjABHa6jACEQxiAoCnoECAAQKg',
         releaseDate: movieDb.releaseDate,
         title: movieDb.title,
         video: movieDb.video,
