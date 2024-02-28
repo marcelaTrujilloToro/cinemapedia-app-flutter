f36c2efdcf99dc536ea5f765bc38e75e5dacd92b
Marcela Trujillo Toro <marces.ttblue@gmail.com>
2024-02-28 17:17:09 -0500

    feat: config dark or light mode

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
+      theme: AppTheme().getTheme(context),
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  ThemeData getTheme() {
+  ThemeData getTheme(BuildContext context) {
+    final Brightness brightnessValue =
+        MediaQuery.of(context).platformBrightness;
+    final bool isDark = brightnessValue == Brightness.dark;
+
     return ThemeData(
       useMaterial3: true,
       colorSchemeSeed: const Color(0XFF2862F5),
+      brightness: isDark ? Brightness.dark : Brightness.light,
     );
   }
 }
